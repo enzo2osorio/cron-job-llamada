49cbba0513d8fd9adc6880235c352ffb0b958e4c
enzo2osorio <168003112+enzo2osorio@users.noreply.github.com>
2025-11-21 16:17:23 +0000

    adding another url for ping

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const https = require("https");
 console.log("🚀 Cron job iniciado");
 
 const url = process.env.MI_API_URL;
+const url_2 = process.env.MI_API_URL_2;
+
 if (!url) {
   console.error("❌ MI_API_URL no está definido");
   process.exit(1);
@@ -17,6 +19,14 @@ https
     process.exit(0); // <-- importante para que el proceso termine
   })
   .on("error", (err) => {
+    console.error("❌ Error en el ping:", err.message);
+    process.exit(1);
+  });
+
+https.get(url_2, (res) => {
+    console.log(`✅ Ping exitoso: ${res.statusCode}`);
+    process.exit(0);
+}) .on("error", (err) => {
     console.error("❌ Error en el ping:", err.message);
     process.exit(1);
   });
